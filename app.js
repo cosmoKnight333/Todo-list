916b5323daf65f6d725b231257e98022f7a0d377
Dev Kumar Seth <devkumar.seth.cd.mat19@itbhu.ac.in>
2021-07-12 17:38:32 +0530

    css added

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
-
-var items = ["utho", "bakchodi karo", "so jaao"];
+mongoose.connect('mongodb://localhost:27017/todo-listDB', { useNewUrlParser: true, useUnifiedTopology: true });
+const itemSchema = new mongoose.Schema({
+    name: String,
+});
 var pdate = new Date();
+var ndate;
 app.get('/', function(req, res) {
-    var ndate = new Date();
+    ndate = new Date();
     var today = new Date();
     let options = {
         weekday: "long",
@@ -25,12 +29,9 @@ app.post('/', function(req, res) {
     var item = req.body.newItem;
     if (item != "") {
         items.push(item);
-        console.log(item);
+
     }
-    if (pdate != ndate) {
-        items = [];
-        pdate = ndate;
-    }
+
     res.redirect('/')
 })
 
